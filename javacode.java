f75470eb16182f463bb8170a2e804ce51b0386bc
ruhi14547 <rutikas.mca25@siescoms.sies.edu.in>
2026-03-08 21:01:33 +0530

    Update javacode.java

--- javacode.java
+++ javacode.java
@@ -3,6 +3,5 @@ public class javacode{
   public static void main(String args[]){
     System.out.println("Hello This is jenkins project")
        System.out.println("Hello Rutika")
-       System.out.println("Updated Code")
   }
 }
