4a706a341a7388340542920c690a83b014310603
ruhi14547 <rutikas.mca25@siescoms.sies.edu.in>
2026-03-11 16:32:56 +0530

    Update javacode.java

--- javacode.java
+++ javacode.java
@@ -3,5 +3,6 @@ public class javacode{
   public static void main(String args[]){
     System.out.println("Hello This is java code");
     System.out.println("Hello This is Webhook");
+    System.out.println("updated Code");
   }
 }
