812af13b7db5c241e5c30f6d057e829ace6597e9
ruhi14547 <rutikas.mca25@siescoms.sies.edu.in>
2026-03-11 16:05:03 +0530

    Update javacode.java

--- javacode.java
+++ javacode.java
@@ -2,5 +2,6 @@ import java.util.*;
 public class javacode{
   public static void main(String args[]){
     System.out.println("Hello This is jenkins project");
+    System.out.println("Hello Webhooks");
   }
 }
